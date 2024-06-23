96a11216c41e6482b2f2c5d91302d1828579af61
ilialksv <iliaalekseevofb@gmail.com>
2024-06-23 15:46:46 +0300

    feat: displayed images are from db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,21 +2,19 @@ import { db } from "@/server/db";
 
 export const dynamic = "force-dynamic";
 
-const mockUrls = [
-  "https://utfs.io/f/d6b6d80c-b433-4228-a6ce-94559a803d92-vb9smn.jpg",
-  "https://utfs.io/f/11bf4f4a-7da4-4fa4-95d8-1102a8c16427-jvgc97.jpg",
-];
-
 export default async function Page() {
-  const posts = await db.query.posts.findMany();
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id)
+  });
 
   return (
     <main className="grid grid-cols-4 gap-4">
-      {posts.map((post) => (
-        <div key={post.id}>{post.name}</div>
-      ))}
-      {mockUrls.map((src, index) => (
-        <img key={index} src={src} alt="Mock image" />
+      {images.map((image) => (
+        <div key={image.id}>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img src={image.url} alt={image.name} />
+          <p>{image.name}</p>
+        </div>
       ))}
     </main>
   );
