aa5fba8212ecc08b4aabd92e834890a657168857
ilialksv <iliaalekseevofb@gmail.com>
2024-06-23 15:34:26 +0300

    feat: page is dynamic, so the posts are always up to date

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import { db } from "@/server/db";
 
+export const dynamic = "force-dynamic";
+
 const mockUrls = [
   "https://utfs.io/f/d6b6d80c-b433-4228-a6ce-94559a803d92-vb9smn.jpg",
   "https://utfs.io/f/11bf4f4a-7da4-4fa4-95d8-1102a8c16427-jvgc97.jpg",
@@ -7,8 +9,6 @@ const mockUrls = [
 
 export default async function Page() {
   const posts = await db.query.posts.findMany();
-
-  console.log(posts);
 
   return (
     <main className="grid grid-cols-4 gap-4">
