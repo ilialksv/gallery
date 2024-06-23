06701f7077874a9b07a0fbf977dad31f2c2b2166
ilialksv <iliaalekseevofb@gmail.com>
2024-06-23 15:25:16 +0300

    feat: vercel postgres db is connected

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
+import { db } from "@/server/db";
+
 const mockUrls = [
   "https://utfs.io/f/d6b6d80c-b433-4228-a6ce-94559a803d92-vb9smn.jpg",
   "https://utfs.io/f/11bf4f4a-7da4-4fa4-95d8-1102a8c16427-jvgc97.jpg",
 ];
 
-export default function Page() {
+export default async function Page() {
+  const posts = await db.query.posts.findMany();
+
+  console.log(posts);
+
   return (
     <main className="grid grid-cols-4 gap-4">
+      {posts.map((post) => (
+        <div key={post.id}>{post.name}</div>
+      ))}
       {mockUrls.map((src, index) => (
         <img key={index} src={src} alt="Mock image" />
       ))}
