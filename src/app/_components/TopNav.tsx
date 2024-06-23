e97559170b6651d81fb29f7bab1e0bbe93020ad1
ilialksv <iliaalekseevofb@gmail.com>
2024-06-23 20:19:00 +0300

    feat: added images upload functionality

--- src/app/_components/TopNav.tsx
+++ src/app/_components/TopNav.tsx
@@ -1,14 +1,24 @@
+'use client';
+
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { UploadButton } from "../_libs/uploadthing";
+import { useRouter } from "next/navigation";
 
 export default function TopNav() {
+  const router = useRouter();
+
   return (
     <nav className="flex justify-between gap-4">
       <h1>Gallery</h1>
-      <div>
+      <div className="flex gap-4">
         <SignedOut>
           <SignInButton />
         </SignedOut>
         <SignedIn>
+          <UploadButton
+            endpoint="imageUploader"
+            onClientUploadComplete={() => router.refresh()}
+          />
           <UserButton />
         </SignedIn>
       </div>
